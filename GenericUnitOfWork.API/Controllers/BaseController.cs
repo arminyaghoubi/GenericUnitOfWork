a9a2c8aa7c4e4e8be6804d721bb4b146f2184035
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2022-12-27 10:53:59 +0330

    #5 has been Created Api Project and Designed a Base Controller for all Controllers

---
+++ GenericUnitOfWork.API/Controllers/BaseController.cs
@@ -0,0 +1,45 @@
+﻿using GenericUnitOfWork.Common.Models;
+using GenericUnitOfWork.Common.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenericUnitOfWork.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public abstract class BaseController<TEntity, TKey, TResult> : ControllerBase
+        where TEntity : BaseModel<TKey>
+        where TResult : class
+    {
+        [HttpGet]
+        public async Task<IPagedList<TResult>> GetPagedListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<TResult> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<TResult> AddAsync(TResult newItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id}")]
+        public async Task UpdateAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
